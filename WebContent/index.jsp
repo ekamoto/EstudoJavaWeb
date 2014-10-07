2d56fb5a8ba59ee1f1a88ab91beff3cad47c64de
Leandro Shindi Ekamoto <ekamoto.leandro@gmail.com>
2014-10-07 14:54:19 -0400

    Estudo JavaWeb

---
+++ WebContent/index.jsp
@@ -0,0 +1,27 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<html>
+<body>
+	Bem vindo ao nosso gerenciador de empresas!
+	<br />
+	 
+	<c:if test="${not empty usuarioLogado}">
+		Logado como: ${usuarioLogado.email} <br>	
+	</c:if>
+	
+	<form action="novaEmpresa" method="POST">
+		Nome: <input type="text" name="nome" /> <input type="submit"
+			value="Enviar">
+	</form>
+
+	<form action="login" method="POST">
+		Email: <input type="text" name="email" value="" /> Senha: <input
+			type="password" name="senha" value="" /> <input type="submit"
+			value="Logar">
+	</form>
+
+	<form action="logout" method="POST">
+
+		<input type="submit" value="Deslogar">
+	</form>
+</body>
+</html>
---
+++ src/main/java/br/com/alura/gerenciador/web/Logout.java
@@ -0,0 +1,32 @@
+package br.com.alura.gerenciador.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.alura.gerenciador.Usuario;
+
+
+
+@WebServlet(urlPatterns="/logout")
+public class Logout extends HttpServlet{
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		req.getSession().setMaxInactiveInterval(60*10);
+		
+		req.getSession().removeAttribute("usuarioLogado");
+		
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/paginas/logout.html");
+		dispatcher.forward(req, resp);
+
+	}
+}
